2273ab73ab607dff179493bee0c2b721b74e5b2e
kaylinPalmisano <kaylin.palmisano@yahoo.com>
2016-08-08 18:08:45 -0500

    Update content_change.js

--- javascripts/content_change.js
+++ javascripts/content_change.js
@@ -5,16 +5,18 @@ var text = document.getElementById("content").scr = "/home/index.html";
 displayContent(name)
 {
   switch(name)
-  case "about": text = document.getElementById("content").scr = "/about/index.html";
-  break;
-  case "trailer": 
-  break;
-  case "resume": text = document.getElementById("content").scr = "/resume/index.html";
-  break;
-  case "portfolio": text = document.getElementById("content").scr = "/portfolio/index.html";
-  break;
-  case "contact": 
-  break;
-  default:
-  break;
+  {
+    case "about": text = document.getElementById("content").scr = "/about/index.html";
+    break;
+    case "trailer": 
+    break;
+    case "resume": text = document.getElementById("content").scr = "/resume/index.html";
+    break;
+    case "portfolio": text = document.getElementById("content").scr = "/portfolio/index.html";
+    break;
+    case "contact": 
+    break;
+    default:
+    break;
+  }
 }
