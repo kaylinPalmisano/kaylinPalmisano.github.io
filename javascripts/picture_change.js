3f7646e8d5a271156e5789af627a2f3e98c1f8cf
kaylinPalmisano <kaylin.palmisano@yahoo.com>
2016-08-07 23:05:24 -0500

    Update picture_change.js

--- javascripts/picture_change.js
+++ javascripts/picture_change.js
@@ -4,7 +4,6 @@ var imgs = ["IMG_7115.jpg", "IMG_7252.jpg"], k=0;
    if (k++ == imgs.length) 
    {
      k=0; 
-     setTimeout('changeImg()', 3000);
    }
  } 
   onload=function(){setTimeout('changeImg()', 3000)}
